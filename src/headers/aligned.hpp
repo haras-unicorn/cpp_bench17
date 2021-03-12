354424bd526499b2fba8facd3123e53d00f83afc
Hrle97 <hrvoje.juric.13@gmail.com>
2021-03-13 00:33:50 +0100

    fixed terminal issues

--- src/headers/aligned.hpp
+++ src/headers/aligned.hpp
@@ -1,47 +1,45 @@
 #include "deps.hpp"
 
-
-template<size_t size, size_t align>
+template <size_t size, size_t align>
 class alignas(align) my_aligned_storage
 {
-    std::byte val[size] {std::byte{0}};
-
+    std::byte val[size]{std::byte{0}};
 
 public:
     constexpr inline my_aligned_storage() noexcept = default;
 
-    template<typename T>
+    template <typename T>
     typename std::enable_if_t<
-            sizeof(T) == size &&
+        sizeof(T) == size &&
             alignof(T) == align &&
             std::is_trivially_copyable_v<T>,
-            my_aligned_storage&>
-    inline operator=(const T& other) noexcept
+        my_aligned_storage &> inline
+    operator=(const T &other) noexcept
     {
         std::memcpy(val, &other, size);
         return *this;
     }
 
-    template<
-            typename T,
-            std::enable_if_t<
-                    sizeof(T) == size &&
-                    alignof(T) == align &&
-                    std::is_trivially_copyable_v<T>,
-                    int> = 0>
-    inline my_aligned_storage(const T& other) noexcept
+    template <
+        typename T,
+        std::enable_if_t<
+            sizeof(T) == size &&
+                alignof(T) == align &&
+                std::is_trivially_copyable_v<T>,
+            int> = 0>
+    inline my_aligned_storage(const T &other) noexcept
     {
         *this = other;
     }
 
-    template<
-            typename T,
-            std::enable_if_t<
-                    sizeof(T) == size &&
-                    alignof(T) == align &&
-                    std::is_trivially_constructible_v<T> &&
-                    std::is_trivially_copyable_v<T>,
-                    int> = 0>
+    template <
+        typename T,
+        std::enable_if_t<
+            sizeof(T) == size &&
+                alignof(T) == align &&
+                std::is_trivially_constructible_v<T> &&
+                std::is_trivially_copyable_v<T>,
+            int> = 0>
     explicit inline operator T() const noexcept
     {
         T dst;
@@ -50,41 +48,43 @@ public:
     }
 };
 
-
-void stack_storage(benchmark::State& state)
+void stack_storage(benchmark::State &state)
 {
     std::array<my_aligned_storage<sizeof(int), alignof(int)>, 100> a{};
 
     for (auto _ : state)
     {
         int i = 1000;
-        for (auto& s : a) s = i++;
+        for (auto &s : a)
+            s = i++;
 
         int sum_bits = 0;
-        for (const auto& s : a) sum_bits += int{s};
+        for (const auto &s : a)
+            sum_bits += int{s};
     }
 }
 
 BENCHMARK(stack_storage);
 
-void stack_int(benchmark::State& state)
+void stack_int(benchmark::State &state)
 {
     std::array<int, 100> ai{};
 
     for (auto _ : state)
     {
         int i = 1000;
-        for (auto& s : ai) s = i++;
+        for (auto &s : ai)
+            s = i++;
 
         int sum = 0;
-        for (const auto& s : ai) sum += s; 
+        for (const auto &s : ai)
+            sum += s;
     }
 }
 
 BENCHMARK(stack_int);
 
-
-void heap_storage(benchmark::State& state)
+void heap_storage(benchmark::State &state)
 {
     std::vector<my_aligned_storage<sizeof(int), alignof(int)>> v{};
     v.reserve(100);
@@ -92,16 +92,18 @@ void heap_storage(benchmark::State& state)
     for (auto _ : state)
     {
         int i = 1000;
-        while (i < 1100) v.push_back(i++);
+        while (i < 1100)
+            v.push_back(i++);
 
         int sum_bits = 0;
-        for (const auto& s : v) sum_bits += int{s};
+        for (const auto &s : v)
+            sum_bits += int{s};
     }
 }
 
 BENCHMARK(heap_storage);
 
-void heap_int(benchmark::State& state)
+void heap_int(benchmark::State &state)
 {
     std::vector<int> vi{};
     vi.reserve(100);
@@ -109,12 +111,13 @@ void heap_int(benchmark::State& state)
     for (auto _ : state)
     {
         int i = 1000;
-        while (i < 1100) vi.push_back(i++);
+        while (i < 1100)
+            vi.push_back(i++);
 
         int sum = 0;
-        for (const auto& s : vi) sum += s; 
+        for (const auto &s : vi)
+            sum += s;
     }
 }
 
 BENCHMARK(heap_int);
-
